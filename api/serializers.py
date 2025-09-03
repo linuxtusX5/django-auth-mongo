5945e44321120fb370840f746da6678a056981f2
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-09-03 18:48:55 +0800

    Minor error

---
+++ api/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import user
+from rest_framework import serializers
+from .models import Note
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at', 'author']
+        extra_kwargs = {'author': {'read_only': True}}
